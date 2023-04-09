8060a0cf10fc71fcb4d09dfa303f053a22325eed
Roman <roma.klimov10.94@gmail.com>
2023-04-09 18:47:57 +1100

    написал вторую программу где используется ввод данных , комичу и отправляю измененную версию на git hub

--- example002_HelloConsole/Program.cs
+++ example002_HelloConsole/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Please , enter your name");
+string username = Console.ReadLine();
+Console.Write("Hi,");
+Console.Write(username);
