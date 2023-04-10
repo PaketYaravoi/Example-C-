dba872938cbcfb0af722e91d5ea33f75641e45a7
Roman <roma.klimov10.94@gmail.com>
2023-04-10 23:19:19 +1100

    example007_magic

--- example003_sum/Program.cs
+++ example003_sum/Program.cs
@@ -1,6 +1,6 @@
-﻿int numberA = new Random().Next(1, 10); // 1 2 3 4...
+﻿int numberA = new Random().Next(1, 20); // 1 2 3 4...
 Console.WriteLine(numberA);
-int numberB = new Random().Next(1, 10);
+int numberB = new Random().Next(1, 1080);
 Console.WriteLine(numberB);
 int resolt = numberA + numberB;
 Console.WriteLine(resolt);
