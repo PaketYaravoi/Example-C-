8a61dc48e8bb078a7a9b70a1d38d6e89dc0eae6a
Roman <roma.klimov10.94@gmail.com>
2023-04-10 13:48:37 +1100

    написал программу нахождения максимального целого числа , из произвольных чисел путем перебора

--- example006_base/Program.cs
+++ example006_base/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿int numberA = 1;
+int numberB = 8;
+int numberC = 13;
+int numberD = 2;
+int numberE = 6;
+
+int max = numberA;
+
+if(numberB > max) max = numberB;
+if(numberC > max) max = numberC;
+if(numberD > max) max = numberD;
+if(numberE > max) max = numberE;
+Console.Write("Max = ");
+Console.WriteLine(max);
